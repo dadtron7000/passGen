24e70bb6dedf6d9a5c7317568825f1e0cb6d4c71
dadtron7000 <kyle.steiner7@gmail.com>
2021-06-22 19:58:04 -0500

    added validation for password length prompt

--- script.js
+++ script.js
@@ -18,119 +18,124 @@ function generatePassword() {
   var numCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var specCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "+"]
   var passLength = prompt("enter a number from 8 to 128");
-  var hasLower = confirm("use lowercase letters?");
-  var hasUpper = confirm("use uppercase letters?");
-  var hasNum = confirm("use numbers?")
-  var hasSpecial = confirm("use special characters?")
-  let output = ``
-  console.log(passLength);
-
-  if (hasLower && hasUpper && hasNum && hasSpecial) {
-    availableCharacters = lowerCharacters.concat(upperCharacters, numCharacters, specCharacters)
-    
-    for(let i = 0; i < passLength; i++) {
-      output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
-    
-    }
+  if (passLength < 8 || passLength > 128){
+    alert("choose valid input")
+    passwordText.value = "Your Secure Password"
+  }else {
+    var hasLower = confirm("use lowercase letters?");
+    var hasUpper = confirm("use uppercase letters?");
+    var hasNum = confirm("use numbers?")
+    var hasSpecial = confirm("use special characters?")
+    let output = ``
   
-  } else if (!hasLower && hasUpper && hasNum && hasSpecial){
-      availableCharacters = upperCharacters.concat(numCharacters, specCharacters)
-
-    for(let i = 0; i < passLength; i++) {
-      output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
-       
-    }   
-  } else if (hasLower && !hasUpper && hasNum && hasSpecial) {
-      availableCharacters = lowerCharacters.concat(numCharacters, specCharacters)
+    
+    if (hasLower && hasUpper && hasNum && hasSpecial) {
+      availableCharacters = lowerCharacters.concat(upperCharacters, numCharacters, specCharacters)
+    
+      for(let i = 0; i < passLength; i++) {
+        output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+    
+      }
+  
+    } else if (!hasLower && hasUpper && hasNum && hasSpecial){
+        availableCharacters = upperCharacters.concat(numCharacters, specCharacters)
 
       for(let i = 0; i < passLength; i++) {
         output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
-    }
-  } else if (hasLower && hasUpper && !hasNum && hasSpecial) {
-      availableCharacters = lowerCharacters.concat(upperCharacters, specCharacters)
+      }   
+    } else if (hasLower && !hasUpper && hasNum && hasSpecial) {
+        availableCharacters = lowerCharacters.concat(numCharacters, specCharacters)
 
-      for(let i = 0; i < passLength; i++) {
-        output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+        for(let i = 0; i < passLength; i++) {
+          output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
-    } 
-  } else if (hasLower && hasUpper && hasNum && !hasSpecial) {
+      }
+    } else if (hasLower && hasUpper && !hasNum && hasSpecial) {
+        availableCharacters = lowerCharacters.concat(upperCharacters, specCharacters)
+
+        for(let i = 0; i < passLength; i++) {
+          output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+       
+      } 
+    } else if (hasLower && hasUpper && hasNum && !hasSpecial) {
       availableCharacters = lowerCharacters.concat(upperCharacters, numCharacters)
 
-      for(let i = 0; i < passLength; i++) {
-        output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+        for(let i = 0; i < passLength; i++) {
+          output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
          
        }
-  } else if (hasLower && hasUpper && !hasNum && !hasSpecial) {
-    availableCharacters = lowerCharacters.concat(upperCharacters)
+    } else if (hasLower && hasUpper && !hasNum && !hasSpecial) {
+      availableCharacters = lowerCharacters.concat(upperCharacters)
 
-    for(let i = 0; i < passLength; i++) {
+      for(let i = 0; i < passLength; i++) {
       output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
      }
-  } else if (hasLower && !hasUpper && hasNum && !hasSpecial) {
-    availableCharacters = lowerCharacters.concat(numCharacters)
+    } else if (hasLower && !hasUpper && hasNum && !hasSpecial) {
+      availableCharacters = lowerCharacters.concat(numCharacters)
 
-    for(let i = 0; i < passLength; i++) {
+      for(let i = 0; i < passLength; i++) {
       output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
      }
-  } else if (hasLower && !hasUpper && !hasNum && hasSpecial) {
-    availableCharacters = lowerCharacters.concat(specCharacters)
+    } else if (hasLower && !hasUpper && !hasNum && hasSpecial) {
+      availableCharacters = lowerCharacters.concat(specCharacters)
 
-    for(let i = 0; i < passLength; i++) {
-      output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
      }
-  } else if (!hasLower && hasUpper && hasNum && !hasSpecial) {
-    availableCharacters = upperCharacters.concat(numCharacters)
+    } else if (!hasLower && hasUpper && hasNum && !hasSpecial) {
+      availableCharacters = upperCharacters.concat(numCharacters)
 
-    for(let i = 0; i < passLength; i++) {
-      output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
      }
-  } else if (!hasLower && hasUpper && !hasNum && hasSpecial) {
-    availableCharacters = upperCharacters.concat(specCharacters)
+    } else if (!hasLower && hasUpper && !hasNum && hasSpecial) {
+      availableCharacters = upperCharacters.concat(specCharacters)
 
-    for(let i = 0; i < passLength; i++) {
-      output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
      }
-  } else if (!hasLower && !hasUpper && hasNum && hasSpecial) {
-    availableCharacters = numCharacters.concat(specCharacters)
+    } else if (!hasLower && !hasUpper && hasNum && hasSpecial) {
+      availableCharacters = numCharacters.concat(specCharacters)
 
-    for(let i = 0; i < passLength; i++) {
-      output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += availableCharacters[Math.floor(Math.random() * availableCharacters.length)]
        
      }
-  } else if (!hasLower && !hasUpper && !hasNum && hasSpecial) {
+    } else if (!hasLower && !hasUpper && !hasNum && hasSpecial) {
 
-    for(let i = 0; i < passLength; i++) {
-      output += specCharacters[Math.floor(Math.random() * specCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += specCharacters[Math.floor(Math.random() * specCharacters.length)]
        
      }
-  } else if (!hasLower && !hasUpper && hasNum && !hasSpecial) {
+    } else if (!hasLower && !hasUpper && hasNum && !hasSpecial) {
 
-    for(let i = 0; i < passLength; i++) {
-      output += numCharacters[Math.floor(Math.random() * numCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += numCharacters[Math.floor(Math.random() * numCharacters.length)]
        
      }
-  } else if (!hasLower && hasUpper && !hasNum && !hasSpecial) {
+    } else if (!hasLower && hasUpper && !hasNum && !hasSpecial) {
 
-    for(let i = 0; i < passLength; i++) {
-      output += upperCharacters[Math.floor(Math.random() * upperCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += upperCharacters[Math.floor(Math.random() * upperCharacters.length)]
        
      }
-  } else if (hasLower && !hasUpper && !hasNum && !hasSpecial) {
+    } else if (hasLower && !hasUpper && !hasNum && !hasSpecial) {
 
-    for(let i = 0; i < passLength; i++) {
-      output += lowerCharacters[Math.floor(Math.random() * lowerCharacters.length)]
+      for(let i = 0; i < passLength; i++) {
+        output += lowerCharacters[Math.floor(Math.random() * lowerCharacters.length)]
        
      }
-  } else {
-    alert("pick valid criteria")
+    } else {
+      alert("enter valid input")
+    }
+    return output
   }
-  return output
 }
 
 // Add event listener to generate button
